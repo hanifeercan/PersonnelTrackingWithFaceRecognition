836f9507c6897fad25a7325dd9a059196cd263d3
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-05-02 19:19:19 +0300

    Develop AdministratorLoginFragment

--- app/src/main/java/com/hercan/personneltrackingwithfacerecognition/administratorlogin/AdministratorLoginFragment.kt
+++ app/src/main/java/com/hercan/personneltrackingwithfacerecognition/administratorlogin/AdministratorLoginFragment.kt
@@ -2,8 +2,12 @@ package com.hercan.personneltrackingwithfacerecognition.administratorlogin
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.hercan.personneltrackingwithfacerecognition.R
 import com.hercan.personneltrackingwithfacerecognition.binding.viewBinding
 import com.hercan.personneltrackingwithfacerecognition.databinding.FragmentAdministratorLoginBinding
@@ -11,15 +15,39 @@ import com.hercan.personneltrackingwithfacerecognition.databinding.FragmentAdmin
 class AdministratorLoginFragment : Fragment(R.layout.fragment_administrator_login) {
     private val
             binding by viewBinding(FragmentAdministratorLoginBinding::bind)
+    private lateinit var auth: FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.tvSignup.setOnClickListener {
             navigateToAdminSingUpFragment()
         }
+        binding.btnLogin.setOnClickListener { login() }
+        auth = Firebase.auth
     }
 
     private fun navigateToAdminSingUpFragment() {
         findNavController().navigate(AdministratorLoginFragmentDirections.navigateToAdministratorSingUpFragment())
+    }
+
+    private fun navigateToHomeFragment() {
+        findNavController().navigate(AdministratorLoginFragmentDirections.navigateToHomeFragment())
+    }
+
+    private fun login() {
+        val email = binding.etMail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        if (email == "") {
+            Toast.makeText(activity, R.string.e_mail_giriniz, Toast.LENGTH_LONG).show()
+        } else if (password == "") {
+            Toast.makeText(activity, R.string.sifre_giriniz, Toast.LENGTH_LONG).show()
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                navigateToHomeFragment()
+            }.addOnFailureListener {
+                Toast.makeText(activity, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hercan/personneltrackingwithfacerecognition/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.hercan.personneltrackingwithfacerecognition.home
+
+import androidx.fragment.app.Fragment
+import com.hercan.personneltrackingwithfacerecognition.R
+
+class HomeFragment : Fragment(R.layout.fragment_home)
