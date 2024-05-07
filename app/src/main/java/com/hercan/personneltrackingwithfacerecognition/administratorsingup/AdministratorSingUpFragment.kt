efd8dad7ba912b0a13ceeb5bbc662b7a50ffafc3
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-05-07 17:23:04 +0300

    Edit AdministratorSingUpFragment

--- app/src/main/java/com/hercan/personneltrackingwithfacerecognition/administratorsingup/AdministratorSingUpFragment.kt
+++ app/src/main/java/com/hercan/personneltrackingwithfacerecognition/administratorsingup/AdministratorSingUpFragment.kt
@@ -8,17 +8,23 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import com.hercan.personneltrackingwithfacerecognition.R
 import com.hercan.personneltrackingwithfacerecognition.binding.viewBinding
 import com.hercan.personneltrackingwithfacerecognition.databinding.FragmentAdministratorSingUpBinding
 
 class AdministratorSingUpFragment : Fragment(R.layout.fragment_administrator_sing_up) {
+
     private val binding by viewBinding(FragmentAdministratorSingUpBinding::bind)
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         auth = Firebase.auth
+        firestore = Firebase.firestore
         binding.btnSingUp.setOnClickListener { signup() }
     }
 
@@ -37,9 +43,19 @@ class AdministratorSingUpFragment : Fragment(R.layout.fragment_administrator_sin
             Toast.makeText(activity, R.string.sifre_giriniz, Toast.LENGTH_LONG).show()
         } else {
             auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
-                login()
-            }.addOnFailureListener {
-                Toast.makeText(activity, it.localizedMessage, Toast.LENGTH_LONG).show()
+
+                val postMap = hashMapOf<String, Any>()
+                postMap["email"] = email
+                postMap["sifre"] = password
+                postMap["yetki"] = "yonetici"
+
+                firestore.collection("sirket").document(email).set(postMap).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        login()
+                    } else {
+                        Toast.makeText(activity, R.string.hata_olustu, Toast.LENGTH_LONG).show()
+                    }
+                }
             }
         }
     }
