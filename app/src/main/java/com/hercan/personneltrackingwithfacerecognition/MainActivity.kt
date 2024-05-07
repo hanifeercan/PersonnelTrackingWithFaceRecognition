057eab6c65e51535efcc51285f82c28c4726fb00
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-05-07 17:35:40 +0300

    Add login check in MainActivity

--- app/src/main/java/com/hercan/personneltrackingwithfacerecognition/MainActivity.kt
+++ app/src/main/java/com/hercan/personneltrackingwithfacerecognition/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.hercan.personneltrackingwithfacerecognition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+        if (isLogged()) {
+            navigateToHomeFragment()
+        }
+    }
+
+    private fun isLogged(): Boolean {
+        val currentUser = auth.currentUser
+        return currentUser != null
+    }
+
+    private fun navigateToHomeFragment() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentContentMain) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.navigate(R.id.homeFragment)
     }
 }
