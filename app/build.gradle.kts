eb07f4c3fe4af48d207331cc557b89be855adf8e
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-05-07 17:37:02 +0300

    Add firebase-firestore dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
 }
