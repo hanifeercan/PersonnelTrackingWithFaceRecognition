ef4bcb30543f3eda9bfb016416a70015cf9aacb5
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2024-11-01 10:11:13 +0300

    Create StrangerTrackingListAdapter

---
+++ app/src/main/java/com/hercan/personneltrackingwithfacerecognition/ui/getstrangertrackingdata/StrangerTrackingListAdapter.kt
@@ -0,0 +1,58 @@
+package com.hercan.personneltrackingwithfacerecognition.ui.getstrangertrackingdata
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hercan.personneltrackingwithfacerecognition.R
+import com.hercan.personneltrackingwithfacerecognition.databinding.ItemStrangerBinding
+import com.squareup.picasso.Picasso
+
+class StrangerTrackingListAdapter :
+    ListAdapter<StrangerTrackingModel, StrangerTrackingListAdapter.ViewHolder>(DiffCallback) {
+
+    inner class ViewHolder(
+        private val binding: ItemStrangerBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: StrangerTrackingModel) = with(binding) {
+
+            if (item.photoUrl != "") {
+                image.setBackgroundResource(0)
+                Picasso.get()
+                    .load(item.photoUrl)
+                    .error(R.drawable.ic_photo)
+                    .placeholder(R.drawable.ic_photo)
+                    .into(image)
+                tvTime.text = item.time
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemStrangerBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        item.let {
+            holder.bind(item)
+        }
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<StrangerTrackingModel>() {
+        override fun areItemsTheSame(
+            oldItem: StrangerTrackingModel, newItem: StrangerTrackingModel
+        ): Boolean {
+            return oldItem.photoUrl == newItem.photoUrl
+        }
+
+        override fun areContentsTheSame(
+            oldItem: StrangerTrackingModel, newItem: StrangerTrackingModel
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
